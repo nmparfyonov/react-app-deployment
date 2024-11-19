9de63a2222a130b5370de75230d263510fae873e
nmparfyonov <nikitamaximovi4@gmail.com>
2024-11-20 00:03:44 +0300

    feat: example application

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://rs.school"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Study at rsschool
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
